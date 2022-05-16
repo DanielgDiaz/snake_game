dea3c5a0a086bf7c110181a704998ec6f9236f53
DanielgDiaz <danielgdiaz92@gmail.com>
2022-05-16 21:02:51 +0200

    init repository   ✨

---
+++ src/model/state.rb
@@ -0,0 +1,14 @@
+module Model
+    class Coord < Struct.new(row, col)
+    end
+
+    class Food < Coord
+    end
+
+    class Snake < Struct.new(positions)
+    end
+
+    class Grid < Struct.new(rows, cols)
+    end
+    
+end
