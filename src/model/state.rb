fb9df4ec4614157136cff66afb2cce73242104a4
DanielgDiaz <danielgdiaz92@gmail.com>
2022-05-22 23:55:10 +0200

    state game_finished

--- src/model/state.rb
+++ src/model/state.rb
@@ -18,15 +18,16 @@ module Model
     class Grid < Struct.new(:rows, :cols)
     end
 
-    class State < Struct.new(:snake, :food, :grid, :next_direction)
+    class State < Struct.new(:snake, :food, :grid, :next_direction, :game_finished)
     end
 
     def self.initial_state
         Model::State.new(
             Model::Snake.new([Model::Coord.new(1,1), Model::Coord.new(0,1)]),
             Model::Food.new(4, 4),
-            Model::Grid.new(8, 12)
-            Direction::DOWN
+            Model::Grid.new(8, 12),
+            Direction::DOWN,
+            false
         )
     end    
 end
